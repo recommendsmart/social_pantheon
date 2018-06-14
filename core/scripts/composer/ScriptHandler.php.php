7f46823cbc6b391cb032fcc891366ddd510799cf
Dayanand <dayanand@kribhcoindia.com>
2018-06-14 17:13:51 +0530

    add easy install module

--- core/scripts/composer/ScriptHandler.php.php
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env php
-<?php
-
-/public static function removeGitDirectories() {
-    $root = static::getDrupalRoot(getcwd());
-    exec('find ' . $root . ' -name \'.git\' | xargs rm -rf');
-  }
