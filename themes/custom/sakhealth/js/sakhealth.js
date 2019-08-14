77faab63b51eefb3c61e3301ffd778c5cde0c9a5
Dayanand <dayanand@kribhcoindia.com>
2019-08-14 11:21:49 +0530

    add skahealth theme

---
+++ themes/custom/sakhealth/js/sakhealth.js
@@ -0,0 +1,5 @@
+/*Custom Responsive Mean Menu Implementation*/
+jQuery('#block-mainnavigation').meanmenu();
+jQuery( "div.view-profiles.view-id-profiles:not(:contains('Profile'))" ).css( "display", "none" );
+
+
