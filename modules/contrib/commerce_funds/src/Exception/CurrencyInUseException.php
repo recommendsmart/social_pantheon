69f2b6fe5509450f7cb202ef488db95d32e85b2e
Dayanand <dayanand@kribhcoindia.com>
2019-10-17 17:35:19 +0530

    remove commerce funds

--- modules/contrib/commerce_funds/src/Exception/CurrencyInUseException.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Drupal\commerce_funds\Exception;
-
-/**
- * Thrown when trying to operate on a currency which exist in user balances.
- */
-class CurrencyInUseException extends \Exception {}
