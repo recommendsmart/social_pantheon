84804b9f8823ccb493701bfb1861c3fffd5a4f62
Dayanand <dayanand@kribhcoindia.com>
2019-10-23 16:53:56 +0530

    add bee

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -90,6 +90,7 @@ class ComposerStaticInitDrupal8
         ),
         'R' => 
         array (
+            'Roomify\\Bat\\' => 12,
             'RRule\\' => 6,
         ),
         'P' => 
@@ -320,6 +321,10 @@ class ComposerStaticInitDrupal8
         'Social\\' => 
         array (
             0 => __DIR__ . '/../..' . '/profiles/contrib/social/src',
+        ),
+        'Roomify\\Bat\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/roomify/bat/src',
         ),
         'RRule\\' => 
         array (
